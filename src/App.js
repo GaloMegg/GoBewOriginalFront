141130695db438b0d7e97da30d8faa47036d64cf
lucasdellamaggiore0 <dellamaggiorelucas25@gmail.com>
2022-05-28 18:18:19 -0300

    Finalizacion de componente de detalle del producto

--- src/App.js
+++ src/App.js
@@ -7,13 +7,14 @@ import { useEffect } from 'react';
 import Nav from './components/nav/Nav';
 import ProductCardContainer from './components/mainContent/ProductCardContainer';
 import CreationForm from './admin/components/products/CreationForm';
+import ProductDetailContainer from './components/productDetail/ProductDetailContainer';
 function App() {
-  
   return (
     <div className="App">
       <Routes>
         <Route exact path='/' element={<><Nav />, <ProductCardContainer /></>} />
         <Route exact path='/admin/new' element={<CreationForm />} />
+        <Route exact path='/productDetail/:id' element={<ProductDetailContainer/>}/>
       </Routes>
     </div>
   );
