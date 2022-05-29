fac088d728743e8adb157e581431e07857d1ffae
WtsCrackin <galomeggiolarobul@gmail.com>
2022-05-29 14:56:57 -0300

    deleting example case

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,26 +1,19 @@
 import { createReducer } from "@reduxjs/toolkit"
-import { ADD_VALUES, GET_CATEGORIES, GET_PRODUCTS, SEARCH_PRODUCT, ORDER_PRODUCTS, GET_PRODUCTS_BYCATEGORY, GET_HIGHLIGHTED, GET_PRODUCT_BY_ID } from "./actions"
+import { GET_CATEGORIES, GET_PRODUCTS, SEARCH_PRODUCT, ORDER_PRODUCTS, GET_PRODUCTS_BYCATEGORY, GET_HIGHLIGHTED, GET_PRODUCT_BY_ID } from "./actions"
 
 const initialState = {
-    value: 0,
     products: [],
     productsToFilter: [],
     product: {},
     categories: []
 }
 export const clientReducer = createReducer(initialState, (builder) => {
-    builder.addCase(ADD_VALUES, (state, action) => {
-        state.value = + action.payload
-    })
-
     builder.addCase(GET_PRODUCT_BY_ID.fulfilled, (state, action) => {
         state.product = action.payload.productList
     })
-
     builder.addCase(GET_PRODUCTS.fulfilled, (state, action) => {
         state.products = action.payload.productList
         state.productsToFilter = action.payload.productList
-
     })
     builder.addCase(SEARCH_PRODUCT.fulfilled, (state, action) => {
         state.products = action.payload
@@ -29,7 +22,6 @@ export const clientReducer = createReducer(initialState, (builder) => {
         console.log(action.payload);
         state.categories = action.payload
     })
-
     builder.addCase(GET_HIGHLIGHTED.fulfilled, (state, action) => {
         console.log(action.payload);
         state.products = action.payload.productList
