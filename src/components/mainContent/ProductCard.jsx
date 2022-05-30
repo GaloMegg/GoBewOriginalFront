247972877e28e67b2aae6df3ad675cf7fb59e6d0
Cristian <cristguevaraolaya@gmail.com>
2022-05-29 19:08:28 -0500

    actualizado

--- src/components/mainContent/ProductCard.jsx
+++ src/components/mainContent/ProductCard.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import styles from '../styles/ProductCard.module.css'
 
 export default function ProductCard({productName, productPrice, productStock, productId}) {
     return (
         
-        <div>
-            <Link to={`/productDetail/${productId}`}>
-                <h2>{productName}</h2>
+        <div className={styles.cardCotainer}>
+            <Link to={`/productDetail/${productId}`} className={styles.link}>
+            <div className={styles.productCard}>
+                <img className={styles.img} src="https://via.placeholder.com/150" alt="product-img" />
+                <h3>{productName}</h3>
+                <p>$ {productPrice.toLocaleString( {style: 'currency',currency: 'INR', minimumFractionDigits: 2})}</p>
+            </div>
             </Link>
-            <img src="https://via.placeholder.com/150" alt="product-img" />
-            <p>{productPrice}</p>
-            <span>{productStock}</span>
-            <p>{productId}</p>
         </div>
     )
 }
