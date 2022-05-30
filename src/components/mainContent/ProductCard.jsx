c62f6a75cbe236b9535e87af90569c3f003afd70
Cristian <cristguevaraolaya@gmail.com>
2022-05-29 19:38:07 -0500

    actualización

--- src/components/mainContent/ProductCard.jsx
+++ src/components/mainContent/ProductCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import styles from '../styles/ProductCard.module.css'
 const { REACT_APP_CLOUDINARY_RES } = process.env
 
 export default function ProductCard({productName, productPrice, productStock, productId, images}) {
@@ -10,11 +11,10 @@ export default function ProductCard({productName, productPrice, productStock, pr
             <div className={styles.productCard}>
                {images?.map((i)=>{
                     if(i.imageIsPrimary){
-                        return <img className={styles.img} src={REACT_APP_CLOUDINARY_RES + i.imageName} alt={i.imageAlt} />
+                        return <img className={styles.img} src={REACT_APP_CLOUDINARY_RES + i.imageName} alt={i.imageAlt} key={i.imageName}/>
                     }
                 })
-            }
-                
+               }
                 <h3>{productName}</h3>
                 <p>$ {productPrice.toLocaleString( {style: 'currency',currency: 'INR', minimumFractionDigits: 2})}</p>
             </div>
