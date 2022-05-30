3396de61a4e51967071646911ac9296f1d7162a6
WtsCrackin <galomeggiolarobul@gmail.com>
2022-05-29 23:07:46 -0300

    giving to the state the quality of conserving the last search or the las filters

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,11 +1,12 @@
 import { createReducer } from "@reduxjs/toolkit"
-import { GET_CATEGORIES, GET_PRODUCTS, SEARCH_PRODUCT, ORDER_PRODUCTS, GET_PRODUCTS_BYCATEGORY, GET_HIGHLIGHTED, GET_PRODUCT_BY_ID } from "./actions"
+import { GET_CATEGORIES, GET_PRODUCTS, SEARCH_PRODUCT, ORDER_PRODUCTS, GET_PRODUCTS_BYCATEGORY, GET_HIGHLIGHTED, GET_PRODUCT_BY_ID, CLEAN_UP_DETAILS } from "./actions"
 
 const initialState = {
     products: [],
     productsToFilter: [],
     product: {},
-    categories: []
+    categories: [],
+    isFiltered: false,
 }
 export const clientReducer = createReducer(initialState, (builder) => {
     builder.addCase(GET_PRODUCT_BY_ID.fulfilled, (state, action) => {
@@ -14,22 +15,26 @@ export const clientReducer = createReducer(initialState, (builder) => {
     builder.addCase(GET_PRODUCTS.fulfilled, (state, action) => {
         state.products = action.payload.productList
         state.productsToFilter = action.payload.productList
+        state.isFiltered = false
     })
     builder.addCase(SEARCH_PRODUCT.fulfilled, (state, action) => {
         state.products = action.payload
+        state.isFiltered = true
     })
     builder.addCase(GET_CATEGORIES.fulfilled, (state, action) => {
-        console.log(action.payload);
         state.categories = action.payload
     })
     builder.addCase(GET_HIGHLIGHTED.fulfilled, (state, action) => {
-        console.log(action.payload);
         state.products = action.payload.productList
     })
     builder.addCase(GET_PRODUCTS_BYCATEGORY, (state, action) => {
         state.products = action.payload
+        state.isFiltered = true
     })
     builder.addCase(ORDER_PRODUCTS, (state, action) => {
         state.products = action.payload
+    })
+    builder.addCase(CLEAN_UP_DETAILS, (state, action) => {
+        state.product = action.payload
     })
 })
