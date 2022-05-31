3c24ba533de9f4529496f0672d5fb751a3addae2
inakielha <inakielhaiek@gmail.com>
2022-05-31 11:35:24 -0300

    app actu

--- src/App.js
+++ src/App.js
@@ -6,10 +6,7 @@ import ProductCardContainer from './components/mainContent/ProductCardContainer'
 import CreationForm from './admin/components/products/CreationForm';
 import ProductDetailContainer from './components/productDetail/ProductDetailContainer';
 import NavBarDetail from './components/productDetail/NavBarDetail';
-<<<<<<< HEAD
 import CreationImage from './admin/components/products/creationImage/CreationImage';
-=======
->>>>>>> 4fa82ddc4c07747e2fa0b26576830dfde8991b1c
 // import CategoriesNew from './components/nav/categories/CategoriesNew.jsx';
 function App() {
 
@@ -20,11 +17,7 @@ function App() {
       <Routes>
         <Route exact path='/' element={<><Nav /> <ProductCardContainer /></>} />
         <Route exact path='/productDetail/:id' element={<><NavBarDetail/> <ProductDetailContainer /> </>} />
-<<<<<<< HEAD
         <Route exact path='/admin/new' element={[<CreationForm />, <CreationImage/>]} />
-=======
-        <Route exact path='/admin/new' element={<CreationForm />} />
->>>>>>> 4fa82ddc4c07747e2fa0b26576830dfde8991b1c
       </Routes>
     </div>
   );
