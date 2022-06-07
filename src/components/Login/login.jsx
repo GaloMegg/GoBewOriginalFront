d955023e692e4245193a5c7a504652e0787f17e0
Soledad Marini <msmarinip@gmail.com>
2022-06-07 19:19:53 -0300

    commit to pull

--- src/components/Login/login.jsx
+++ src/components/Login/login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import validate from './Validate.js';
+import validate from './validate.js';
 import { POST_USER, CLEAN_USER_RESPONSE } from '../../redux/actions';
 import { Link, useNavigate } from 'react-router-dom';
 import LogInGoogle from './LogInGoogle.jsx';
