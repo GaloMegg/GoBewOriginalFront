a7f42471f0c5e1cd008764f50bf951c8233292d7
inakielha <inakielhaiek@gmail.com>
2022-06-06 21:20:56 -0300

    log in mejorado, manejo de usuario invalido

--- src/components/Login/LogInGoogle.jsx
+++ src/components/Login/LogInGoogle.jsx
@@ -9,7 +9,7 @@ export default function LogInGoogle() {
     const dispatch = useDispatch()
 
     function handleCalBackResponse(response) {
-        console.log("Encoded JWT ID Token: " + response.credential)
+        // console.log("Encoded JWT ID Token: " + response.credential)
         const userObject = jwt_decode(response.credential)
         console.log(userObject)
         setuser(userObject);
@@ -33,16 +33,18 @@ export default function LogInGoogle() {
 
     useEffect(() => {
         /* global google */
-        google?.accounts?.id?.initialize({
-            client_id: "730053348546-b9gt1dk3ja161r1ndcjrc2v8gkfoalfi.apps.googleusercontent.com",
-            callback: handleCalBackResponse
-        })
-        google?.accounts?.id?.renderButton(
-            document.getElementById("signInDiv"),
-            { theme: "outline", size: "large" }
-        )
-    }, []);
-
+        if (window.google) {
+            google.accounts.id.initialize({
+                client_id: "730053348546-b9gt1dk3ja161r1ndcjrc2v8gkfoalfi.apps.googleusercontent.com",
+                callback: handleCalBackResponse
+            })
+            google.accounts.id.renderButton(
+                document.getElementById("signInDiv"),
+                { theme: "outline", size: "large" }
+                )
+            }
+            }, []);
+            
     return (
         <div className="googleForm">
             <div id="signInDiv" className="googleForm--signIn"></div>
