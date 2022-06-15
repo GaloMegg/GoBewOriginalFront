3e822debcf53c77baaf99c4f02e4edcc56d49012
genafalconi <genaro.f@hotmail.com>
2022-06-15 12:57:40 -0300

    Confl

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./scss/main.scss";
 import Nav from './components/nav/Nav';
 import ProductDetailContainer from './components/productDetail/ProductDetailContainer';
 import CartContainer from './components/cart/CartContainer';
-import Login from './components/login/Login'
+import Login from './components/login/Login';
 import CreateUserForm from './components/login/CreateUserForm';
 import { ToastContainer } from 'react-toastify';
 import MainContentContainer from './components/mainContent/MainContentContainer';
